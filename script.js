5647c99b6755cd0916adb12283cd70095445b219
ApostolOleg <apostololeg0@gmail.com>
2025-10-28 13:56:46 +0200

    small changes

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-const quizContainer = document.getElementById("quiz-container");
+const container = document.getElementById("container");
 
-fetch("quizes.json")
-	.then((response) => response.json())
-	.then((data) => {
-		data.quizes.forEach((quiz) => {
-			const quizElement = document.createElement("div");
-			quizElement.classList.add("quiz");
-			quizElement.innerHTML = `
-                <h2>${quiz.title}</h2>
-                <p>${quiz.description}</p>
-            `;
-			quizContainer.appendChild(quizElement);
-		});
-	})
-	.catch((error) => console.error("Error fetching quiz data:", error));
