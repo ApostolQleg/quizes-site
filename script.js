ca4b24e106028167f5e9ae53d41ed5b35dca75a6
ApostolOleg <apostololeg0@gmail.com>
2025-10-29 17:00:26 +0200

    renaming and creating structure

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const container = document.getElementById("container");
+const createBtn = document.getElementById("create");
+const manageBtn = document.getElementById("manage");
+const resultsBtn = document.getElementById("results");
 
-// function to show all quizes in the container
+// button functionality
+createBtn.addEventListener("click", function () {
+	// Create button functionality
+	window.location.href = "create/create.html";
+});
 
+manageBtn.addEventListener("click", function () {
+	// Manage button functionality
+	window.location.href = "manage/manage.html";
+});
+
+resultsBtn.addEventListener("click", function () {
+	// Results button functionality
+	window.location.href = "results/results.html";
+});
