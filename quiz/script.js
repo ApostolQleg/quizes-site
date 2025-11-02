621d6b641365fd9b2462ee72f867617c56fa0b8e
Dmytrii Penkivskiy <dmitrijpenkivskij@gmail.com>
2025-11-02 21:33:45 +0000

    tried something (need to talk to Oleg)

--- quiz/script.js
+++ quiz/script.js
@@ -1,7 +1,23 @@
-const Quiz1 = {
-	header: "Це перший тест", // назва тесту
-	questions: {
-		q1: "Запитання",
-		answ1: "Відповідь",
-	},
-};
+  const correctAnswer = "A";
+
+  const submitBtn = document.getElementById("submit");
+  const result = document.getElementById("result");
+  const form = document.getElementById("quiz-form");
+
+  submitBtn.addEventListener("click", () => {
+    const selected = form.answer.value; // отримаємо вибраний варіант
+
+    if (!selected) {
+      result.textContent = "Вибери варіант відповіді!";
+      result.style.color = "orange";
+      return;
+    }
+
+    if (selected === correctAnswer) {
+      result.textContent = "Правильно";
+      result.style.color = "green";
+    } else {
+      result.textContent = "Неправильно";
+      result.style.color = "red";
+    }
+  });
