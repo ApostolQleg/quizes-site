299d0a20f806814614bc7f2a8dbfcbddf29839e4
ApostolOleg <apostololeg0@gmail.com>
2025-11-10 21:06:41 +0200

    piska added

--- script.js
+++ script.js
@@ -20,12 +20,43 @@ quizzes.forEach((quiz) => {
 
 		// create modal to start quiz
 		//window.location.href = "/quiz";
-		if (ifPressed) {
+		if (!ifPressed) {
+			// ensure the container is a positioning context
+			if (!container.style.position) container.style.position = "relative";
+
+			// add dark overlay
+			const overlay = document.createElement("div");
+			overlay.id = "overlay";
+			document.body.appendChild(overlay);
+
+			// animate the button to fill the container
+			button.style.position = "absolute";
+			button.style.width = "80%";
+			button.style.height = "80%";
+			button.style.top = "10%";
+			button.style.left = "10%";
+			button.style.border = "10px solid rgba(104, 25, 84, 1)";
+			button.style.zIndex = "10";
+
+			// add button to start quiz
+			const startQuizButton = document.createElement("button");
+			startQuizButton.id = "start-quiz-button";
+			startQuizButton.innerText = "Start Quiz";
+
+			startQuizButton.onclick = () => {
+				window.location.href = "/quiz";
+			};
+			button.appendChild(startQuizButton);
+		} else {
 			// ensure the container is a positioning context
 			if (!container.style.position) container.style.position = "relative";
 
 			// reset position
-			button.style.position = "stati";
+			button.style.position = "static";
+
+			// remove dark overlay
+			overlay = document.getElementById("overlay");
+			overlay.remove();
 
 			// animate the button back to original size
 			button.style.width = "20vw";
@@ -36,26 +67,6 @@ quizzes.forEach((quiz) => {
 			if (document.getElementById("start-quiz-button")) {
 				button.removeChild(document.getElementById("start-quiz-button"));
 			}
-		} else {
-			// ensure the container is a positioning context
-			if (!container.style.position) container.style.position = "relative";
-
-			// animate the button to fill the container
-			button.style.position = "absolute";
-			button.style.width = "80%";
-			button.style.height = "80%";
-			button.style.top = "10%";
-			button.style.left = "10%";
-			button.style.border = "10px solid rgba(104, 25, 84, 1)";
-			// add button to start quiz
-			const startQuizButton = document.createElement("button");
-			startQuizButton.id = "start-quiz-button";
-			startQuizButton.innerText = "Start Quiz";
-
-			startQuizButton.onclick = () => {
-				window.location.href = "/quiz";
-			};
-			button.appendChild(startQuizButton);
 		}
 		ifPressed = !ifPressed;
 	});
