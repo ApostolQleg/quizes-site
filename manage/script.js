552a3828dce10e5d6260c0d649ccf5285b1e55b4
IVANNYKOV VLADYSLAV <79372738+gun-flud@users.noreply.github.com>
2025-11-12 22:30:34 +0000

    new work

--- manage/script.js
+++ manage/script.js
@@ -1,7 +1,66 @@
 const container = document.querySelector(".container");
 const storage = JSON.parse(localStorage.getItem("storage"));
-
+const saveQuizButton = document.getElementById("saveQuizButton");
 // перевірка який квіз натиснули для редагування
-const title = storage.quizzes.selected.title;
-console.log(title);
+const title = storage.selected.title;
+const currentQuiz = storage.quizzes.find((quiz) => quiz.title === title);
+//
+const quizTitle = document.querySelector(".quizTitle");
+const quizDescriptionInput = document.querySelector(".quizDescriptionInput");
+const optionsCount = currentQuiz.questions.length;
+const charCount = document.querySelector('.charCount');
 
+quizTitle.value = currentQuiz.title;
+//counter quizTitle
+let quizTitleLenght = quizTitle.value.length;
+charCount.textContent = `${quizTitleLenght}/30`;
+quizTitle.addEventListener('input', () => {
+	quizTitleLenght = quizTitle.value.length;
+	charCount.textContent = `${quizTitleLenght}/30`;
+});
+
+quizDescriptionInput.value = currentQuiz.description;
+
+for (let question of currentQuiz.questions) {
+    const questionTitle = question.text;
+
+    const create = document.createElement("div");
+    create.className = "quizzesContainer";
+    create.innerHTML = `<button class="material-symbols-outlined" id="deletequestion" class="deletequestion">delete</button>
+		<p class="questionNum">Запитання №</p>
+				<input type="text" class="questionText" placeholder="Текст питання" value = '${questionTitle}'/>
+				<p class="variant">Варіанти відповіді</p>
+				<style>
+					.material-symbols-outlined {
+						font-variation-settings: "FILL" 0, "wght" 400, "GRAD" 0, "opsz" 24;
+					}
+				</style>
+				<!-- Тут будуть відображатися створені опитування -->
+				 <button class="addvariant" id="addvariant">+</button>`;
+    container.appendChild(create);
+    saveQuizButton.before(create);
+    for (let option of question.options) {
+        let prev = create.querySelector(".addvariant");
+        let optionVal = document.createElement("label");
+        optionVal.className = "options";
+        optionVal.innerHTML = `<label>
+					<input type="checkbox" name="option1" value="checked" class="isCorrect" /><input
+						type="text"
+						class="optionText"
+						placeholder="Варіант відповіді"
+						value="${option.text}"
+					/>
+                    <button class="material-symbols-outlined" id="deletevariant">delete</button>
+				</label>`;
+        // якщо потрібно, щоб option був вибраний
+        const checkbox = optionVal.querySelector(".isCorrect");
+
+        if (option.isCorrect) {
+            checkbox.checked = true;
+        }
+        prev.before(optionVal);
+        console.log(prev);
+    }
+}
+
+// console.log(optionsCount);
